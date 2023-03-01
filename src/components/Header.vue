7d80c3e123ee8783aa8e6b3060f48dc31cd8f50f
MedinaSekiraqa <102260547+MedinaSekiraqa@users.noreply.github.com>
2023-03-02 00:48:53 +0100

    First Commit

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+
+import { title } from 'process';
+
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                title:'New HerLine site',
+            }
+        }
+    }
+</script>
+
+<style>
+    h1{
+        font-size: 2rem;
+    }
+</style>
